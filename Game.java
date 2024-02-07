5dbb8a2b669390b45d2a004d589ccdaf5ac300a1
aayushmandavya <mandavyaaayush98@gmail.com>
2024-02-07 12:26:14 +0545

    first commit

---
+++ Game.java
@@ -0,0 +1,41 @@
+package FirstGame;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JFrame;
+import java.io.File;
+
+public class Game {
+    private static JFrame window;
+    private static GamePanel gamePanel;
+    public Game() {
+    }
+    public void main() {
+        try {
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(new File("backgroundMusic.wav")));
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+//        JFrame window = new JFrame("Shooting Game");
+//        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//        window.pack();
+//        window.setContentPane(new GamePanel());
+//        window.setResizable(false);
+//        window.setVisible(true);
+//        window.pack();
+        window = new JFrame("Shooting Game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        gamePanel = new GamePanel(this);  // Pass the Game instance to the GamePanel
+        window.setContentPane(gamePanel);
+        window.setResizable(false);
+        window.pack();
+        window.setVisible(true);
+
+    }
+    // Method to restart the game
+
+}
